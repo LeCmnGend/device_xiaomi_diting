83642c89f54fb661ae535f204de089babffb871b
Joey <joey@evolution-x.org>
2026-01-17 16:18:14 +0900

    sm8450-common: Patch libaudiocloudctrl to depend on libtinyxml2-v34.so

--- extract-files.py
+++ extract-files.py
@@ -93,6 +93,7 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
         .regex_replace('.+dolby.+\n', ''),
     (
+        'vendor/lib64/libaudiocloudctrl.so',
         'vendor/lib64/libdpps.so',
         'vendor/lib64/libsnapdragoncolor-manager.so',
     ): blob_fixup()
